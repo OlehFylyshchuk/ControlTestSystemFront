a4efee3cefdb6f92335ce596c2e6dec711b82152
OlehFylyshchuk <oleh.fylyshchuk-ist201@nung.edu.ua>
2024-02-10 14:23:09 +0200

    update

--- ControlTestSystemFront/src/app/app.module.ts
+++ ControlTestSystemFront/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -30,7 +31,6 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-
     HttpClientModule
   ],
   providers: [authInterceptorProviders],
