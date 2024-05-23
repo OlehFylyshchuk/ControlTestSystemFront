a1555b548d53c02eb80770fecab100ab8e4ae120
OlehFylyshchuk <oleh.fylyshchuk-ist201@nung.edu.ua>
2024-05-23 15:05:13 +0300

    add host & 2 remote project

---
+++ apps/admin/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, NxWelcomeComponent],
+  selector: 'cts-admin-entry',
+  template: `<cts-nx-welcome></cts-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -3,6 +3,18 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'admin',
+    loadChildren: () => import('admin/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('auth/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'student',
+    loadChildren: () => import('student/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
